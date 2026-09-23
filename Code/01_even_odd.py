f5845d117e28c8c258cbfb57b8c63c084a4e3f59
prachiy1029-ux <prachiy1029@gmail.com>
2026-09-23 22:29:36 +0530

    Add even odd program

---
+++ Code/01_even_odd.py
@@ -0,0 +1,10 @@
+def check_even_odd(number):
+    if number % 2 == 0:
+        return "Even"
+    else:
+        return "Odd"
+
+
+if __name__ == "__main__":
+    number = int(input("Enter a number: "))
+    print(check_even_odd(number))
