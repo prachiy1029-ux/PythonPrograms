fafed18f1bdbaaf9210e8211a9f8b00eab508cf0
prachiy1029-ux <prachiy1029@gmail.com>
2026-09-24 16:45:54 +0530

    Add sum of digit

---
+++ Code/10_sum_of_digits.py
@@ -0,0 +1,15 @@
+def sum_of_digits(number):
+    number = abs(number)
+    total = 0
+
+    while number > 0:
+        digit = number % 10
+        total += digit
+        number //= 10
+
+    return total
+
+
+if __name__ == "__main__":
+    number = int(input("Enter a number: "))
+    print("Sum of digits:", sum_of_digits(number))
